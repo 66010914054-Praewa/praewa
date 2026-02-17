58ca7ed5983b52cb0ecaa16520957ab8a373c6e5
Praewa <66010914054@msu.ac.th>
2026-02-17 19:42:16 +0700

    a

---
+++ i/connectdb.php
@@ -0,0 +1,8 @@
+<?php
+		$host = "localhost";
+		$user = "root";
+		$pwd = "666";
+		$db = "4054db";
+		$conn = mysqli_connect($host, $user, $pwd, $db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
+		mysqli_query($conn, "SET NAMES utf8");
+?>
