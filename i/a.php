e8b386a7d813fd6069a239a6e8bb46f3a4b3d07a
Praewa <66010914054@msu.ac.th>
2026-02-17 21:27:30 +0700

    1

--- i/a.php
+++ i/a.php
@@ -2,52 +2,45 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>66010914054 แพรวา ชาวดอน (แพร)</title>
+<title>แพรวา ชาวดอน (แพร)</title>
 </head>
 
 <body>
 
-<h1>งาน i --  แพรวา ชาวดอน (แพร)</h1>
+<h1>งาน i -- แพรวา ชาวดอน (แพร)</h1>
 
-<form method="post" action="">
-    ชื่อภาค <input type="text" name="rname" autofocus required>
+<form method="post" action=" ">
+	ชื่อภาค <input type="text" name="rname" autofocus required>
     <button type="submit" name="Submit">บันทึก</button>
-</form><br><br>
+</form> <br><br>
 
 <?php 
-if(isset($_POST['Submit'])) {
-    include_once("connectdb.php");
-    $rname = $_POST['rname'];
-    $sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL, '{$rname}')";
-    mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");
-   
+if(isset($_POST['Submit'])){
+	include_once("connectdb.php");
+	$rname = $_POST['rname'];
+	$sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL, '{$rname}')";
+	mysqli_query($conn,$sql2) or die ("เพิ่มข้อมูลไม่ได้");
 }
 ?>
 
-<?php
-include_once("connectdb.php");
-$sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn, $sql);
-
-?>
-
 <table border="1">
-    <tr>
-        <th>รหัสภาค</th>
+	<tr>
+    	<th>รหัสภาค</th>
         <th>ชื่อภาค</th>
         <th>ลบ</th>
-    </tr>
+  </tr>
 <?php
 include_once("connectdb.php");
 $sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn, $sql);
+$rs = mysqli_query($conn,$sql);
 while ($data = mysqli_fetch_array($rs)){
 ?>
     <tr>
-        <td><?php echo $data['r_id']; ?> </td>
+    	<td><?php echo $data['r_id']; ?> </td>
         <td><?php echo $data['r_name']; ?> </td>
-        <td width="80" align="center"><a href="delete_regions.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.jpg" width="20"></td>
-    </tr>
+        <td width="80" align="center"><a href="delete_region.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ');"><img src="images/delete.jpg" width="20"></a></td>
+	</tr>
+
 <?php } ?>
 </table>
 
@@ -55,7 +48,5 @@ while ($data = mysqli_fetch_array($rs)){
 </html>
 
 <?php
-mysqli_close($conn); 
+mysqli_close($conn);
 ?>
-</body>
-</html>
