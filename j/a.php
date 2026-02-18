585013eef34b771f5fdb554c9c66b2643ddfb99d
praewa <66010914054@msu.ac.th>
2026-02-18 14:06:28 +0700

    1

---
+++ j/a.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>66010914054 แพรวา ชาวดอน(แพร)</title>
+</head>
+
+<body>
+<h1>งาน j - แพรวา ชาวดอน(แพร)</h1>
+<td>
+	<tr>
+<img src="images/1.jpg" height="400">
+</tr>
+</td>
+</body>
+</html>
